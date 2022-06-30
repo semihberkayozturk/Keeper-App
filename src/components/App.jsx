dabd9ac80d2c361f63e818b6773e53c0efa884f1
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-06-30 15:46:00 +0300

    complete part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Note from "./Note.jsx";
+import notes from "../notes";
+
 
 function App() {
-    return <div >
-        <Header / >
-        <Note />
-        <Footer / >
-        </div>
-};
+    return (
+    <div>
+        <Header />
+            {notes.map(noteItems => <Note
+        key={noteItems.key}
+        title={noteItems.title}
+        content={noteItems.content}
+    />  
+)};
+        <Footer/>
+    </div>
+)};
 
 export default App;
